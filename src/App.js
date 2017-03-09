f286f27389d02042e7cc8e0d7b814a342e5860cc
Jon Chan <jchan@stackoverflow.com>
2017-03-09 11:17:00 -0500

    Added basic components for twitter feed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Tweet extends Component {
+  render() {
+    return (
+      <div className="tweet">
+        <p>{this.props.body}</p>
+        <p>{this.props.userId}</p>
+      </div>
+      )
+  }
+}
+
+var Feed = React.createClass({
+  getInitialState: function() {
+    return {
+      tweets: [
+        {
+          "id": 1,
+          "userId": 1,
+          "body": "React is awesome!",
+          "noOfLikes": 24,
+          "datePosted": "2017-03-22 12:00:22"
+        },
+        {
+          "id": 2,
+          "userId": 1,
+          "body": "...so is React Native!",
+          "noOfLikes": 12,
+          "datePosted": "2017-03-22 12:00:22"
+        }
+      ]
+    }
+  },
+  render: function() {
+    var tweets = [];
+    for (var key in this.state.tweets) {
+          var tweet = this.state.tweets[key]
+          tweets.push(
+            <Tweet body={tweet.body} userId={tweet.userId} />
+          )
+        }
+    console.log(tweets);
+    return (
+      <div>
+        <p>Hello World</p>
+        {tweets}
+      </div>
+    )
+  }
+}
+)
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hi there</p>
+        <Feed />
       </div>
     );
   }
