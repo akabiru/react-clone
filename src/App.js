b7e1e6776b00b1484aae5dffa1175409fb03d7fc
Austin Kabiru <austin.kabiru@andela.com>
2017-03-09 19:29:05 +0300

    user handle

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ class Tweet extends Component {
   render() {
     return (
       <div className="tweet">
+        <p>{this.props.user.handle}</p>
         <p>{this.props.body}</p>
-        <p>{this.props.userId}</p>
       </div>
-      )
+    )
   }
 }
 
 var Feed = React.createClass({
-  getInitialState: function() {
+  getInitialState: function () {
     return {
       tweets: [
         {
@@ -31,17 +31,48 @@ var Feed = React.createClass({
           "noOfLikes": 12,
           "datePosted": "2017-03-22 12:00:22"
         }
+      ],
+      users: [
+        {
+          "id": 1,
+          "handle": "@mark",
+          "name": "Mark Zuck"
+        },
+        {
+          "id": 2,
+          "handle": "@austin",
+          "name": "Austin Kabiru"
+        },
+        {
+          "id": 1,
+          "handle": "@jon",
+          "name": "Jon Chan"
+        }
       ]
     }
   },
-  render: function() {
+  render: function () {
     var tweets = [];
     for (var key in this.state.tweets) {
-          var tweet = this.state.tweets[key]
-          tweets.push(
-            <Tweet body={tweet.body} userId={tweet.userId} />
-          )
+      var tweet = this.state.tweets[key]
+      var user;
+
+      for (var idx in this.state.users) {
+        if ( this.state.users[idx].id == tweet.userId) {
+          user = this.state.users[idx]
+          break
         }
+      }
+
+      tweets.push(
+        <Tweet body={tweet.body} user={user} />
+      )
+    }
+
+    function getUser() {
+
+    }
+
     console.log(tweets);
     return (
       <div>
