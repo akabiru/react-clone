62959f8dc4c3925de97e6188cdd7cc89b70a7d9d
Jon Chan <me@jonhmchan.com>
2017-03-21 10:57:03 -0400

    Added tweet form

--- src/App.js
+++ src/App.js
@@ -14,42 +14,31 @@ class Tweet extends Component {
   }
 }
 
+var SubmitForm = React.createClass({
+  onSubmit: function(event) {
+    event.preventDefault()
+    this.props.onAddTweet(this.refs.tweet.value)
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input type="text" ref="tweet" />
+        <input type="submit" />
+      </form>
+    )
+  }
+})
+
 var Feed = React.createClass({
-  componentWillMount: function () {
-    var that = this;
-    Axios.get('http://localhost:4000/tweets')
-      .then(function(tweets) {
-        console.log('Ther er: ', tweets.data);
-        that.setState({ tweets: tweets.data });
-      })
-      .catch(function(error) {
-        // handle error
-      })
-    
-    Axios.get('http://localhost:4000/users')
-      .then(function(users) {
-        console.log('Users: ', users.data);
-        that.setState({ users: users.data })
-      })
-      .catch(function(error) {
-        // handle error
-      })
-  },
-  getInitialState: function () {
-    return {
-      tweets: [],
-      users: []
-    }
-  },
   render: function () {
     var tweets = [];
-    for (var key in this.state.tweets) {
-      var tweet = this.state.tweets[key]
+    for (var key in this.props.tweets) {
+      var tweet = this.props.tweets[key]
       var user;
 
-      for (var idx in this.state.users) {
-        if ( this.state.users[idx].id == tweet.userId) {
-          user = this.state.users[idx]
+      for (var idx in this.props.users) {
+        if ( this.props.users[idx].id == tweet.userId) {
+          user = this.props.users[idx]
           break
         }
       }
@@ -74,15 +63,54 @@ var Feed = React.createClass({
 }
 )
 
-class App extends Component {
+var App = React.createClass({
+  componentWillMount() {
+    var that = this;
+    Axios.get('http://localhost:4000/tweets')
+      .then(function(tweets) {
+        console.log('Ther er: ', tweets.data);
+        that.setState({ tweets: tweets.data });
+      })
+      .catch(function(error) {
+        // handle error
+      })
+    
+    Axios.get('http://localhost:4000/users')
+      .then(function(users) {
+        console.log('Users: ', users.data);
+        that.setState({ users: users.data })
+      })
+      .catch(function(error) {
+        // handle error
+      })
+  },
+  addTweet(tweet) {
+    this.setState({
+      tweets: this.state.tweets.concat([
+        {
+          "id": this.state.tweets.length + 1,
+          "userId": 1,
+          "body": tweet,
+          "noOfLikes": 0,
+          "datePosted": ""
+        }
+      ])
+    })
+  },
+  getInitialState() {
+    return {
+      tweets: [],
+      users: []
+    }
+  },
   render() {
     return (
       <div className="App">
-        <p>Hi there</p>
-        <Feed />
+        <SubmitForm users={this.state.users} onAddTweet={this.addTweet} />
+        <Feed users={this.state.users} tweets={this.state.tweets} />
       </div>
     );
   }
-}
+})
 
 export default App;
